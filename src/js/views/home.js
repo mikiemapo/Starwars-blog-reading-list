094a9be232646dbaf50759119667527b934a908b
mikiemapo <123407631+mikiemapo@users.noreply.github.com>
2023-05-05 23:33:58 +0000

    save

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -9,11 +9,11 @@ export const Home = () => {
 	const [dropdown,setDropdown]= useState()
 	return (
 	<div className="container-fluid text-center mt-5">
-		<h1 className="card-text fs-1">People</h1>
+		<h1 className="card-text font-monospace fw-bold fs-1">People</h1>
 		<People/>
-		<h1 className="card-text fs-1">Planet</h1>
+		<h1 className="card-text font-monospace fw-bold  fs-1">Planet</h1>
 		<Planet />
-		<h1 className="card-text fs-1">Vehicle</h1>
+		<h1 className="card-text font-monospace fw-bold  fs-1">Vehicle</h1>
 		<Vehicle />
 	</div>
 	)
